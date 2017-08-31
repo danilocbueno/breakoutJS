e1a439500115fab31ee957a8b4776ff3de65b846
danilocbueno <danilocbueno@gmail.com>
2017-08-31 01:25:34 +0000

    detecção de colisões simples

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ var xBola = canvas.width/2;
 var yBola = canvas.height-30;
 var dxBola = 2;
 var dyBola = -2;
+var raioBola = 20;
 
 //atribuindo altura e largura
 canvas.width = 480;
@@ -13,13 +14,24 @@ canvas.height = 320;
 //pegando o contexto (lápis)
 var ctx = canvas.getContext("2d");
 
-function desenhar() {
+function desenharBola() {
 	ctx.beginPath();
-	ctx.arc(xBola, yBola, 10, 0, Math.PI*2);
+	ctx.arc(xBola, yBola, raioBola, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
+}
 
+function desenhar() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	desenharBola();
+	if(yBola < raioBola || yBola > canvas.height - raioBola) {
+		dyBola = -dyBola;
+	}
+
+	if(xBola < raioBola || xBola > canvas.width - raioBola) {
+		dxBola = -dxBola;
+	}
 	xBola = xBola + dxBola;
 	yBola = yBola + dyBola;
 }
